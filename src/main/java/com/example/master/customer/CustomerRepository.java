a3cb66b09d62908a006c874901816dffd2212a19
Oğuz Evrensel <oevrensel@gmail.com>
2022-09-24 17:43:30 +0300

    Section 2 has completed

--- src/main/java/com/example/master/customer/CustomerRepository.java
+++ src/main/java/com/example/master/customer/CustomerRepository.java
@@ -1,4 +1,22 @@
 package com.example.master.customer;
 
-public class CustomerRepository {
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+@Primary
+public class CustomerRepository implements CustomerRepo{
+    @Override
+    public List<Customer> getCustomers() {
+
+        // TODO CONNECT TO REAL DB
+        return Collections.singletonList(
+                new Customer(1L, "TODO: Implemet real db")
+
+        );
+    }
 }
