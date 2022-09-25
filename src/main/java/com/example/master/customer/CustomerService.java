1f1de30efc9bbbd4b684c545f3fd50951f61ad51
Oğuz Evrensel <oevrensel@gmail.com>
2022-09-25 19:30:04 +0300

    Section 5 has completed

--- src/main/java/com/example/master/customer/CustomerService.java
+++ src/main/java/com/example/master/customer/CustomerService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -17,8 +18,13 @@ public class CustomerService {
     }
 
 
-    List<Customer> getCustomer() {
+    List<Customer> getCustomers() {
 
         return customerRepo.getCustomers();
+    }
+
+    Customer getCustomer(Long id) {
+        return getCustomers().stream().filter(customer -> customer.getId().equals(id)).findFirst()
+                .orElseThrow(() -> new IllegalStateException("customer not found"));
     }
 }
