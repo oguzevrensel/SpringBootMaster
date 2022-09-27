143a800adf2331f8bd82c0239800a6e24566521b
Oğuz Evrensel <oevrensel@gmail.com>
2022-09-27 22:55:04 +0300

    Section 10 has completed

--- src/main/java/com/example/master/customer/CustomerService.java
+++ src/main/java/com/example/master/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.example.master.customer;
 
 import com.example.master.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
@@ -11,6 +13,8 @@ import java.util.List;
 
 @Service
 public class CustomerService {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
     private final CustomerRepository customerRepository;
     @Autowired
     public CustomerService(
@@ -20,12 +24,21 @@ public class CustomerService {
 
 
     List<Customer> getCustomers() {
-
+        LOGGER.info("getCustomers was called");
         return customerRepository.findAll();
     }
 
     Customer getCustomer(Long id) {
+
         return customerRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("customer with id " + id + " not found"));
+                .orElseThrow(
+                        () -> {
+                            NotFoundException notFoundException = new NotFoundException(
+                                    "customer with id " + id + " not found"
+                            );
+                    LOGGER.error("error in getting customer {}", id, notFoundException);
+                    return notFoundException;
+
+                });
     }
 }
