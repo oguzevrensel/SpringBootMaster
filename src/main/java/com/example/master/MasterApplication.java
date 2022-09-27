180a784d602e4a2c33e79be21d39e7c93701c975
Oğuz Evrensel <oevrensel@gmail.com>
2022-09-27 21:20:52 +0300

    Section 9 has completed

--- src/main/java/com/example/master/MasterApplication.java
+++ src/main/java/com/example/master/MasterApplication.java
@@ -2,8 +2,10 @@ package com.example.master;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MasterApplication {
 
 	public static void main(String[] args) {
